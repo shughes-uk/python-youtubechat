4d096ca8cf7ddb180d30bc2a95d12e6bd2bcaa5a
shughes <shughes.uk@gmail.com>
2016-01-03 20:12:55 -0600

    uninope

--- youtubechat/ytchat.py
+++ youtubechat/ytchat.py
@@ -11,6 +11,7 @@ from datetime import datetime, timedelta
 import threading
 from pprint import pformat
 from HTMLParser import HTMLParser
+import re
 
 html_parser = HTMLParser()
 
@@ -73,8 +74,8 @@ class LiveChatMessage(object):
         self.id = json['id']
         snippet = json['snippet']
         self.type = snippet['type']
-        self.message_text = html_parser.unescape(snippet['textMessageDetails']['messageText'].encode('UTF-8'))
-        self.display_message = html_parser.unescape(snippet['displayMessage'].encode('UTF-8'))
+        self.message_text = html_parser.unescape(snippet['textMessageDetails']['messageText']).encode('ascii','ignore')
+        self.display_message = html_parser.unescape(snippet['displayMessage']).encode('ascii','ignore')
         self.has_display_content = snippet['hasDisplayContent']
         self.live_chat_id = snippet['liveChatId']
         self.published_at = get_datetime_from_string(snippet['publishedAt'])
